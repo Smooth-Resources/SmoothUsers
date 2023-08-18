bcaa0a482f18f82ed785e17126bf9f4312a876ef
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-08-18 20:49:26 +0200

    Add player quit listener

---
+++ src/main/java/net/smoothplugins/smoothusers/listener/PlayerQuitListener.java
@@ -0,0 +1,25 @@
+package net.smoothplugins.smoothusers.listener;
+
+import com.google.inject.Inject;
+import net.smoothplugins.smoothusers.SmoothUsers;
+import net.smoothplugins.smoothusersapi.user.UserService;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerQuitListener implements Listener {
+
+    @Inject
+    private UserService userService;
+    @Inject
+    private SmoothUsers plugin;
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+            userService.setTTLOfCacheByUUID(event.getPlayer().getUniqueId(), 600); // 10 minutes
+            userService.setTTLOfCacheByUsername(event.getPlayer().getName(), 600); // 10 minutes
+        });
+    }
+}
--- src/main/java/net/smoothplugins/smoothusers/loader/ListenerLoader.java
+++ src/main/java/net/smoothplugins/smoothusers/loader/ListenerLoader.java
@@ -3,6 +3,7 @@ package net.smoothplugins.smoothusers.loader;
 import com.google.inject.Inject;
 import net.smoothplugins.smoothusers.SmoothUsers;
 import net.smoothplugins.smoothusers.listener.PlayerJoinListener;
+import net.smoothplugins.smoothusers.listener.PlayerQuitListener;
 import org.bukkit.Bukkit;
 
 public class ListenerLoader {
@@ -11,8 +12,11 @@ public class ListenerLoader {
     private SmoothUsers smoothUsers;
     @Inject
     private PlayerJoinListener playerJoinListener;
+    @Inject
+    private PlayerQuitListener playerQuitListener;
 
     public void load() {
         Bukkit.getPluginManager().registerEvents(playerJoinListener, smoothUsers);
+        Bukkit.getPluginManager().registerEvents(playerQuitListener, smoothUsers);
     }
 }
